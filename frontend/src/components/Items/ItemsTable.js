a536211373d1ae63fde6dbd63cd18b0b7ae8e5cc
mehdi ayoub <ayoub.mehdi@outlook.com>
2023-09-28 18:28:18 +0300

    added the add new item

--- frontend/src/components/Items/ItemsTable.js
+++ frontend/src/components/Items/ItemsTable.js
@@ -5,6 +5,8 @@ function ItemsTable() {
     const [items, setItems] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
     const [editingItem, setEditingItem] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [newSerialNumber, setNewSerialNumber] = useState("");
 
     const productTypeId = useParams().productTypeId;
 
@@ -27,92 +29,121 @@ function ItemsTable() {
             });
     }, [productTypeId]);
 
-
     const handleSoldChange = (itemId) => {
-      const itemToUpdate = items.find(item => item.id === itemId);
-      const updatedStatus = !itemToUpdate.sold;
+        const itemToUpdate = items.find(item => item.id === itemId);
+        const updatedStatus = !itemToUpdate.sold;
 
-      // Update the URL here
-      fetch(`http://localhost:3000/api/v1/product_types/${productTypeId}/items/${itemId}`, {
-          method: 'PATCH',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ sold: updatedStatus })
-      })
-      .then(res => {
-          if (res.ok) {
-              return res.json();
-          } else {
-              throw new Error('Failed to update sold status.');
-          }
-      })
-      .then(updatedItem => {
-          setItems(prevItems => prevItems.map(item => item.id === itemId ? updatedItem : item));
-      });
+        fetch(`http://localhost:3000/api/v1/product_types/${productTypeId}/items/${itemId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ sold: updatedStatus })
+        })
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error('Failed to update sold status.');
+            }
+        })
+        .then(updatedItem => {
+            setItems(prevItems => prevItems.map(item => item.id === itemId ? updatedItem : item));
+        });
     };
 
-
-
     const handleEdit = (item) => {
-      console.log("productTypeId:", productTypeId);
-      // console.log("editingItem:", editingItem);
-
         setEditingItem(item);
     };
 
     const handleSaveChanges = () => {
-      if (editingItem) {
-        const url = `http://localhost:3000/api/v1/product_types/${productTypeId}/items/${editingItem.id}`;
-
-        console.log("PATCH URL:", url);
-        console.log("Editing Item:", editingItem);
-
-        // Make a PATCH request to the API with the updated serial number
-        fetch(url, {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ item: { serial_number: editingItem.serial_number } }) // Here is the update
-        })
-        .then(res => {
-          if (res.ok) {
-            console.log("Server Response:", res);
-            setItems(prevItems => prevItems.map(item => item.id === editingItem.id ? editingItem : item));
-            setEditingItem(null); // Exit edit mode
-          } else {
-            throw new Error('Failed to update the item');
-          }
-        })
-        .catch(error => {
-          console.error('Error updating item:', error);
-        });
-      }
+        if (editingItem) {
+            const url = `http://localhost:3000/api/v1/product_types/${productTypeId}/items/${editingItem.id}`;
+            fetch(url, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ item: { serial_number: editingItem.serial_number } })
+            })
+            .then(res => {
+                if (res.ok) {
+                    setItems(prevItems => prevItems.map(item => item.id === editingItem.id ? editingItem : item));
+                    setEditingItem(null); // Exit edit mode
+                } else {
+                    throw new Error('Failed to update the item');
+                }
+            })
+            .catch(error => {
+                console.error('Error updating item:', error);
+            });
+        }
     };
 
     const handleDelete = (itemId) => {
-      fetch(`http://localhost:3000/api/v1/product_types/${productTypeId}/items/${itemId}`, { method: 'DELETE' })
-          .then(res => {
-              if (res.ok) {
-                  return res.json();
-              } else {
-                  throw new Error('Failed to delete item');
-              }
-          })
-          .then(() => {
-              setItems(prevItems => prevItems.filter(item => item.id !== itemId));
-          })
-          .catch(error => {
-              console.error('Error deleting item:', error);
-          });
-  };
+        fetch(`http://localhost:3000/api/v1/product_types/${productTypeId}/items/${itemId}`, { method: 'DELETE' })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw new Error('Failed to delete item');
+                }
+            })
+            .then(() => {
+                setItems(prevItems => prevItems.filter(item => item.id !== itemId));
+            })
+            .catch(error => {
+                console.error('Error deleting item:', error);
+            });
+    };
 
+    const handleAddNewItem = () => {
+      fetch(`http://localhost:3000/api/v1/product_types/${productTypeId}/items`, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ item: { serial_number: newSerialNumber } }) // Updated this line
+      })
+      .then(res => {
+          if (!res.ok) {
+              return res.json().then(err => {
+                  throw new Error(err.message || 'Failed to add the new item');
+              });
+          }
+          return res.json();
+      })
+      .then(newItem => {
+          setItems(prevItems => [...prevItems, newItem]);
+          setModalOpen(false);
+          setNewSerialNumber('');
+      })
+      .catch(error => {
+          console.error('Error adding new item:', error);
+      });
+    };
 
     if (!isLoaded) return <div>Loading...</div>;
 
     return (
         <div>
+            <button onClick={() => setModalOpen(true)}>Add New Item</button>
+
+            {modalOpen && (
+                <div>
+                    <h3>Add New Item</h3>
+                    <label>
+                        Serial Number:
+                        <input
+                            value={newSerialNumber}
+                            onChange={(e) => setNewSerialNumber(e.target.value)}
+                        />
+                    </label>
+                    <button onClick={handleAddNewItem}>Submit</button>
+                    <button onClick={() => setModalOpen(false)}>Cancel</button>
+                </div>
+            )}
+
             <table>
                 <thead>
                     <tr>
@@ -138,12 +169,12 @@ function ItemsTable() {
                                 <input type="checkbox" checked={item.sold} onChange={() => handleSoldChange(item.id)} />
                             </td>
                             <td>
-                              {editingItem && editingItem.id === item.id ? (
-                                <button onClick={handleSaveChanges}>Save</button>
-                              ) : (
-                                <button onClick={() => handleEdit(item)}>Edit</button>
-                              )}
-                              <button onClick={() => handleDelete(item.id)}>Delete</button>
+                                {editingItem && editingItem.id === item.id ? (
+                                    <button onClick={handleSaveChanges}>Save</button>
+                                ) : (
+                                    <button onClick={() => handleEdit(item)}>Edit</button>
+                                )}
+                                <button onClick={() => handleDelete(item.id)}>Delete</button>
                             </td>
                         </tr>
                     ))}
