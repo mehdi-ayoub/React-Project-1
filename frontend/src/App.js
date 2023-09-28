89b607265f9e83f9994a47de724822106ef331c8
mehdi ayoub <ayoub.mehdi@outlook.com>
2023-09-28 23:03:34 +0300

    added some front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Import BrowserRouter and other necessary components
-// import ProductTypesTable from './components/ProductTypes/ProductTypesTable';
-// import ItemsTable from './components/Items/ItemsTable'; // Import the ItemsTable component
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Product Types</h1>
-//       <Router>
-//         <Switch>
-//           <Route path="/items">
-//             <ItemsTable />
-//           </Route>
-//           <Route path="/">
-//             <ProductTypesTable />
-//           </Route>
-//         </Switch>
-//       </Router>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
 import React from 'react';
