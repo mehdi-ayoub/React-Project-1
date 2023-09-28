bd6fb7cf258eae046bab6d9261fd5654841156b3
mehdi ayoub <ayoub.mehdi@outlook.com>
2023-09-28 18:56:26 +0300

    testing the master

--- frontend/src/components/ProductTypes/ProductTypesTable.js
+++ frontend/src/components/ProductTypes/ProductTypesTable.js
@@ -27,7 +27,7 @@ function ProductTypesTable() {
             setCsrfToken(tokenElement.getAttribute('content'));
         }
     }, []);
-
+    // to edit the item
     const handleEditClick = (productType) => {
         setCurrentProductType(productType);
         setEditedProductType(productType);
