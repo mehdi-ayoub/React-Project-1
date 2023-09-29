c23d7be5c9759e8f8e4ff1d12a6e15eb93783b2c
mehdi ayoub <ayoub.mehdi@outlook.com>
2023-09-29 06:41:34 +0300

    added some styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Im
 
 import ProductTypesTable from './components/ProductTypes/ProductTypesTable';
 import ItemsTable from './components/Items/ItemsTable';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar /> {/* Include the Navbar component */}
         <Routes> {/* Use Routes instead of Switch */}
           <Route path="/" element={<ProductTypesTable />} /> {/* Use element prop */}
           <Route path="/items/:productTypeId" element={<ItemsTable />} /> {/* Use element prop */}
