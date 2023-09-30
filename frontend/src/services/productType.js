a824a61160ab439e71e3f127ba7621a12b7e6eab
mehdi ayoub <ayoub.mehdi@outlook.com>
2023-09-30 12:31:43 +0300

    refactored

--- frontend/src/services/productType.js
+++ frontend/src/services/productType.js
@@ -1,27 +1,27 @@
 import api from './base'
 
 export const fetchProductsRequest = async () => {
-    return await api.get(`/product_types`);
+  return await api.get(`/product_types`);
 }
 
 export const deleteProductRequest = async (id) => {
-    return await api.delete(`/product_types/${id}`);
+  return await api.delete(`/product_types/${id}`);
 }
 
 export const editProductRequest = async (id, data, csrfToken) => {
-    return await api.put(`/product_types/${id}`, data, {
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': csrfToken
-        }
-    });
+  return await api.put(`/product_types/${id}`, data, {
+      headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': csrfToken
+      }
+  });
 }
 
 export const addProductRequest = async (data, csrfToken) => {
-    return await api.post(`/product_types`, data, {
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': csrfToken
-        }
-    });
+  return await api.post(`/product_types`, data, {
+      headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': csrfToken
+      }
+  });
 }
